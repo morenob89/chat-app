14694bae37860b57767382cd44844a05b8769c93
morenob89 <moreno.balajic@gmail.com>
2022-07-20 20:17:28 +0200

    Updated left and right writing

--- src/components/Message/Message.component.jsx
+++ src/components/Message/Message.component.jsx
@@ -2,10 +2,15 @@ import "./Message.styles.scss";
 
 import { Avatar } from "../Avatar";
 import { MessageText } from "../MessageText";
+import { useUser } from "../../contexts/UserContext";
 
 export function Message(props) {
+
+  const user = useUser();
+
+
   return (
-    <div className="message">
+    <div className={`message ${user.user.displayName === props.displayName ? 'my-messages' : ''}`}>
       <div className="message__avatar">
         <Avatar
           backgroundColor={props.avatarBackgroundColor}
